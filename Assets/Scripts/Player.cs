1df190161d186d930b452ddcfa5c345eab904ada
Joshua Novak <jdjnovak@gmail.com>
2019-04-27 00:17:12 -0400

    Sliced sprite sheet, added player movment and states.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour { 
+
+    public enum States {
+        FullCharge,
+        HighCharge,
+        HalfCharge,
+        LowCharge,
+        NoCharge,
+        Dead = -1
+    }
+
+    public float maxVelocity;
+    public float currentVelocity;
+    public float moveBuffer; // For controller use
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+    public KeyCode leftKey;
+    public KeyCode rightKey;
+
+    private SpriteRenderer sr;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() { 
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        MovementUpdate();
+    }
+
+    void Init() {
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void MovementUpdate() {
+        //rb.AddForce(new Vector2(maxVelocity * Input.GetAxis("Horizontal"), maxVelocity * Input.GetAxis("Vertical")));
+        if (Utilities.GetHypo(rb.velocity) <= maxVelocity) {
+            rb.velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal") * maxVelocity, 0.8f),
+                                      Mathf.Lerp(0, Input.GetAxis("Vertical") * maxVelocity, 0.8f));
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour
+{
+    public static float GetHypo(Vector2 v) {
+        return Mathf.Sqrt(v.x*v.x + v.y*v.y);
+    }
+}
